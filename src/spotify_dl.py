7c201b907fcd04a72f95f70b184a0b255886e81e
MattJaccino <mattjacc98@yahoo.com>
2024-08-20 17:14:11 -0400

    add more lucida formats, build

--- src/spotify_dl.py
+++ src/spotify_dl.py
@@ -70,11 +70,14 @@ DOWNLOADER_SPOTIFYDOWN_HEADERS = {
 DOWNLOADER_LUCIDA = "lucida"
 DOWNLOADER_LUCIDA_URL = "https://lucida.to"
 DOWNLOADER_LUCIDA_FILE_FORMATS = [
+    'original',
     'mp3-320', 'mp3-256', 'mp3-128',
     'ogg-vorbis-320', 'ogg-vorbis-256', 'ogg-vorbis-128',
     'ogg-opus-320', 'ogg-opus-256', 'ogg-opus-128', 'ogg-opus-96', 'ogg-opus-64',
     'opus-320', 'opus-256', 'opus-128', 'opus-96', 'opus-64',
-    'bitcrush', 'original'
+    'flac-16',
+    'm4a-aac-320', 'm4a-aac-256', 'm4a-aac-192', 'm4a-aac-128',
+    'bitcrush'
 ]
 DOWNLOADER_LUCIDA_FILE_FORMAT_DEFAULT = "mp3-320"
 DOWNLOADER_LUCIDA_HEADERS = {
